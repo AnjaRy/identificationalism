5225812d7e9adcd0f7ef210a70f0f8ed5c931199
AnjaRy <anja.ryser.sh@gmail.com>
2018-12-17 09:24:41 +0100

    changes on final version of randomsearch and added program to find tfidf_max

---
+++ tfidf_max.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.model_selection import RandomizedSearchCV
+from baseline import Trainer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+
+
+def randomsearch(max):
+	my_trainer = Trainer(data='csv/train.csv')
+	my_trainer._preprocess()
+
+	# bigger search space
+	knn = KNeighborsClassifier()
+	k_nearest = list(range(30,33))
+
+	# define parameters whose value space needs to be searched
+	param_grid = {'n_neighbors': k_nearest}
+
+	random_search = RandomizedSearchCV( estimator=knn,
+        	                            param_distributions=param_grid,
+                	                    n_iter=3,
+                        	           # zehn zufällige möglichkeiten
+                                	    cv=10,
+                                    	scoring='accuracy',
+                                    	n_jobs=3,
+                                    	return_train_score=True,)
+
+
+	# fit iris data (reload if yu modified those variables)
+	y = my_trainer.train_y
+	vec=TfidfVectorizer(strip_accents='ascii', preprocessor=None, tokenizer=word_tokenize, max_df=max)
+	X = vec.fit_transform(my_trainer.train_X)
+
+	random_search.fit(X, y)
+
+	pd.options.display.max_colwidth = 100 
+
+	df = pd.DataFrame.from_dict(random_search.cv_results_)
+	df = df.sort_values(by=["rank_test_score"])
+	df_relevant = df[['rank_test_score', 'mean_test_score', 'params',  'mean_fit_time']]
+
+	print(df)
+	print(df_relevant)
+	print(random_search.best_params_)
+	print('NEXT')
+
+if __name__ == '__main__': 
+	for i in np.linspace(0.7,1,7):
+		print(i)
+		randomsearch(i)
+
+
+#stop_words=stopwords.words("english")
